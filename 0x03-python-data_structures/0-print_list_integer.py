3fcd59021ed39842005796cce78a6643d4c63132
tsucess <taofeeq.muhammad22@gmail.com>
2023-06-12 07:53:33 -0700

    checker error fixed

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,6 +2,5 @@
 # 0-print_list_integer.py
 
 def print_list_integer(my_list=[]):
-        """Print all integers of a list"""
         for x in range(len(my_list)):
-            print("{:d}".format(my_list[x]))
+            print("{:d}".format(x))

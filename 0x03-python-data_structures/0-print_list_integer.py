f835d59e99b6ee0aa114119dc4c0dae7ea0ebf45
tsucess <taofeeq.muhammad22@gmail.com>
2023-06-12 07:39:35 -0700

    Task 0 print list integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,5 @@
 
 def print_list_integer(my_list=[]):
         """Print all integers of a list"""
-        for x in len(my_list):
+        for x in range(len(my_list)):
             print("{}".format(my_list[x]))
